c7baf78de95f8b5610b7904390199f41697144cd
Muhammad Yahaya <adelekeyahaya05@gmail.com>
2025-03-04 17:29:43 +0100

    Add initial implementation of email validator and tests

---
+++ src/EmailValidator.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Muhammad\CompanyEmailValidator;
+
+class EmailValidator
+{
+    private array $freeEmailProviderDomains;
+
+    public function __construct(private array $userDefinedFreeEmailProviderDomains = [])
+    {
+        $defaultDomains = json_decode(file_get_contents(__DIR__ . '/data/free_email_provider_domains.json'), true);
+        
+        // Merge built-in domains with user-provided ones
+        $this->freeEmailProviderDomains = array_merge($defaultDomains, $userDefinedFreeEmailProviderDomains);
+    }
+
+    private function validate(string $email): bool
+    {
+        return filter_var($email, FILTER_VALIDATE_EMAIL) !== false;
+    }
+
+    public function isCompanyEmail(string $email): bool
+    {
+        if (!$this->validate($email)) {
+            return false;
+        }
+
+        $domain = explode('@', $email)[1];
+
+        return !in_array($domain, $this->freeEmailProviderDomains);
+    }
+    
+    /**
+     * Validate multiple emails at once.
+     *
+     * @param array $emails List of emails to validate.
+     * @return array Associative array with email as key and validation result as value.
+     */
+    public function areCompanyEmails(array $emails): array
+    {
+        $results = [];
+
+        foreach ($emails as $email) {
+            $results[$email] = $this->isCompanyEmail($email);
+        }
+
+        return $results;
+    }
+}
